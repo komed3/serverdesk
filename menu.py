ba2faf53a9acb2c0211e7cf875e5468fe6d16fa5
komed3 <pk97@outlook.de>
2025-07-09 17:47:26 +0200

    refactor menu.py: unify command key in actions.json and enhance touch input handling

--- menu.py
+++ menu.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
 
+import evdev # type: ignore
 import json
 import os
+import pygame # type: ignore
 import subprocess
+import time
 
-# Paths
+# Paths & constants
 SRC_PATH = os.path.dirname( os.path.realpath( __file__ ) )
 CFG_PATH = os.path.join( SRC_PATH, 'cfg' )
 BIN_PATH = os.path.join( SRC_PATH, 'bin' )
+IMG_PATH = os.path.join( SRC_PATH, 'assets' )
+
+TIMEOUT_SEC = 4
 
 # Available menu actions
 actions = []
@@ -20,12 +26,20 @@ def load_actions():
     with open( os.path.join( CFG_PATH, 'actions.json' ) ) as f:
         return json.load( f )
 
+# Find the suitable action by position
+def find_action( x: int, y: int ) -> ( str | None ):
+    for a in actions:
+        if ( a[ 'x1' ] <= x <= a[ 'x2' ] and
+             a[ 'y1' ] <= y <= a[ 'y2' ] ):
+            return a[ 'cmd' ]
+    return None
+
 # Resolve command (replace %DIR% with source path)
-def resolve_command( cmd: str ):
+def resolve_command( cmd: str ) -> str:
     return cmd.replace( '%DIR%', SRC_PATH )
 
 # Terminate current running process if there is one
-def terminate_proc():
+def terminate_proc() -> None:
     global proc
     if proc:
         try:
@@ -36,18 +50,54 @@ def terminate_proc():
         proc = None
 
 # Run command as sub process
-def run_command( cmd: str ):
+def run_command( cmd: str ) -> None:
     global proc
     terminate_proc()
     proc = subprocess.Popen(
-        cmd, shell = True,
+        resolve_command( cmd ),
+        shell = True,
         start_new_session = True
     )
 
-# Find the suitable action by position
-def find_action( x: int, y: int ):
-    for a in actions:
-        if ( a[ 'x1' ] <= x <= a[ 'x2' ] and
-             a[ 'y1' ] <= y <= a[ 'y2' ] ):
-            return a
-    return None
+# The main program
+def main() -> None:
+    global actions
+    x = y = last_touch = None
+
+    # Load available actions
+    actions = load_actions()
+
+    # Initiate pygame for menu overlay
+    pygame.init()
+    pygame.mouse.set_visible( False )
+    img = pygame.image.load( os.path.join( IMG_PATH, 'menu.png' ) )
+    screen = pygame.display.set_mode( ( 0, 0 ), pygame.FULLSCREEN )
+    screen.blit( img, ( 0, 0 ) )
+    pygame.display.flip()
+
+    # Get the touch device
+    device = evdev.InputDevice( '/dev/input/event3' )
+    device.grab()
+
+    # Main loop
+    for e in device.read_loop():
+        if e.type == evdev.ecodes.EV_ABS:
+            if e.code == evdev.ecodes.ABS_X:
+                x = e.value * 1024 / 4096
+            elif e.code == evdev.ecodes.ABS_Y:
+                y = e.value *  600 / 4096
+        elif ( e.type == evdev.ecodes.EV_KEY and
+               e.code == evdev.ecodes.BTN_TOUCH and
+               e.value == 1 ):
+            if x is not None and y is not None:
+                action = find_action( x, y )
+                if action:
+                    run_command( action )
+                last_touch = time.time()
+        elif last_touch and ( time.time() - last_touch > TIMEOUT_SEC ):
+            screen.blit( img, ( 0, 0 ) )
+            pygame.display.flip()
+            last_touch = None
+
+# Run the program
+main()
