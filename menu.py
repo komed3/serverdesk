4711b229ae4bd0215af64df87ef7b5481390da3f
komed3 <pk97@outlook.de>
2025-07-09 12:53:36 +0200

    add `find_action` method

--- menu.py
+++ menu.py
@@ -3,10 +3,14 @@
 import os
 import subprocess
 
+# Available menu actions
+actions = None
+
+# Stores the current (sub) process
 proc = None
 
 # Terminate current running process if there is one
-def terminate_proc ():
+def terminate_proc():
     global proc
     if proc:
         try:
@@ -17,10 +21,17 @@ def terminate_proc ():
         proc = None
 
 # Run command as sub process
-def run_command ( cmd ):
+def run_command( cmd ):
     global proc
     terminate_proc()
     proc = subprocess.Popen(
         cmd, shell = True,
         preexec_fn = os.setsid
     )
+
+# Find the suitable action by position
+def find_action( x, y ):
+    for a in actions:
+        if a[ 'x1' ] <= x <= a[ 'x2' ] and a[ 'y1' ] <= y <= a[ 'y2' ]:
+            return a
+    return None
