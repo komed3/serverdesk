4c65ab0621152d1f129facf5ce0f4aea5b777e7c
komed3 <pk97@outlook.de>
2025-07-09 23:16:49 +0200

    add step 3 to 6

--- bin/upgrade.sh
+++ bin/upgrade.sh
@@ -5,7 +5,7 @@
 
 # Colors
 RESET='\033[0m'
-GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
 CYAN='\033[1;36m'
 
 # Helper function to display a countdown before proceeding
@@ -27,16 +27,43 @@ countdown () {
 }
 
 # Step 1 – Update package sources
-echo -e "${GREEN}=== Step 1: Update package sources ===${RESET}"
+echo -e "${YELLOW}=== Step 1: Update package sources ===${RESET}"
 apt update
 
 countdown 0
 
 # Step 2 - Show list of upgradable packages
-echo -e "${GREEN}=== Step 2: Packages that need to be updated ===${RESET}"
+echo -e "${YELLOW}=== Step 2: Packages that need to be updated ===${RESET}"
 apt list --upgradable 2>/dev/null | tail -n +2
 
 # Wait for some time depending on the number of packages to be updated
 updates_count=$( apt list --upgradable 2>/dev/null | wc -l )
 wait_time=$(( updates_count < 2 ? 3 : ( updates_count > 10 ? 10 : updates_count ) ))
 countdown $wait_time
+
+# Step 3 – Upgrade
+echo -e "${YELLOW}=== Step 3: Upgrade installed packages ===${RESET}"
+apt upgrade -y
+
+countdown 0
+
+# Step 4 – Distribution upgrade
+echo -e "${YELLOW}=== Step 4: System upgrade (distribution) ===${RESET}"
+apt dist-upgrade -y
+
+countdown 5
+
+# Step 5 – Autoremove not used packages
+echo -e "${YELLOW}=== Step 5: Remove packages no longer needed ===${RESET}"
+apt autoremove -y
+
+countdown 0
+
+# Step 6 – Clean up package cache
+echo -e "${YELLOW}=== Step 6: Clean up package cache ===${RESET}"
+apt clean
+
+countdown 0
+
+# Finishing
+echo -e "${YELLOW}=== System has been successfully upgraded ===${RESET}"
