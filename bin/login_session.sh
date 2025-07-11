99a034fb19a25272b2fde722703e90215300f34d
komed3 <pk97@outlook.de>
2025-07-11 10:33:00 +0200

    Update login_session.sh

--- bin/login_session.sh
+++ bin/login_session.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Switch to login TTY
-sudo chvt 3
+chvt 3
 
 # Wait briefly for a clean switchover
 sleep 0.2
 
 # Start login (blocking)
 exec </dev/tty3 > /dev/tty3 2>&1
-sudo setsid login
+setsid login
 RET=$?
 
 # After logout: switch back to the ServerDesk console
-sudo chvt 1
+chvt 1
 
 # Show errors
 exit $RET
