0307bc208f5780bdac98a1da7d21ffb3fc080a44
komed3 <pk97@outlook.de>
2025-07-11 16:42:57 +0200

    stop service when leaving tty1

--- bin/session.sh
+++ bin/session.sh
@@ -5,14 +5,3 @@ sudo chvt 3
 
 # Interrupt ServerDesk
 sudo systemctl stop serverdesk.service
-
-# Wait until a user logs out
-#while who | grep -q 'tty3'; do
-#    sleep 1
-#done
-
-# Switch back to TTY1
-#sudo chvt 1
-
-# Restart ServerDesk
-#sudo systemctl restart serverdesk.service
